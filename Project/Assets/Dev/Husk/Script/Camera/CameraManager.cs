4889f7c7ccc990224e3697f039c3bcf13df25bcb
husk <huskgit@gmail.com>
2021-09-25 02:35:03 +0900

    탭 기능 UI 연결

---
+++ Project/Assets/Dev/Husk/Script/Camera/CameraManager.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using TMPro;
+
+public class CameraManager : MonoBehaviour
+{
+    public bool isInGame;
+    float shakeTimer;
+    float totalShakeTimer;
+    float startingIntensity;
+    [SerializeField] CinemachineVirtualCamera InGameCam;
+    [SerializeField] CinemachineVirtualCamera TimelineCam;
+    public CinemachineConfiner InGameConfiner;
+    public CinemachineConfiner TimeLineConfiner;
+
+    [Header("UI 관련")]
+    public bool tabbed;
+    [SerializeField] GameObject UI;
+    [SerializeField] TextMeshProUGUI tabCountUI;
+    void Awake()
+    {
+        isInGame = true;
+        tabbed = false;
+    }
+
+    public void ChangeCam(Collider2D border)
+    {
+        if(isInGame && !tabbed)
+        {
+            TimeLineConfiner.m_BoundingShape2D = border;
+            isInGame = false;
+            UI.SetActive(false);
+            tabbed = true;
+            tabCountUI.text = "You Can't Tab!";
+            TimelineCam.gameObject.SetActive(true);
+            InGameCam.gameObject.SetActive(false);
+
+
+            //instance.ShakeCamera(0f, 0f);
+        }
+        else
+        {
+            InGameConfiner.m_BoundingShape2D = border;
+            isInGame = true;
+            UI.SetActive(true);
+            InGameCam.gameObject.SetActive(true);
+            TimelineCam.gameObject.SetActive(false);
+
+
+            //instance.ShakeCamera(0f, 0f);
+        }
+    }
+
+    public void ShakeCamera(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin multiChannelPerlin;
+        if(isInGame)
+        {
+            multiChannelPerlin = InGameCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        }
+        else 
+        {
+            multiChannelPerlin = TimelineCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        }
+        
+        multiChannelPerlin.m_AmplitudeGain = intensity;
+
+        startingIntensity = intensity;
+        totalShakeTimer = time;
+        shakeTimer = time;
+    }
+
+    private void Update() 
+    {
+        if(shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            if(shakeTimer <= 0f)
+            {   
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin;
+                if(isInGame)
+                {
+                    cinemachineBasicMultiChannelPerlin = InGameCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                }
+                else
+                {
+                    cinemachineBasicMultiChannelPerlin = TimelineCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                }
+                
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 
+                    Mathf.Lerp(startingIntensity, 0f, (1 - (shakeTimer / totalShakeTimer)));
+            }
+        }
+    }
+}
+
+
+/*
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager instance;
+    public bool isInGame;
+    float shakeTimer;
+    float totalShakeTimer;
+    float startingIntensity;
+    [SerializeField] CinemachineVirtualCamera InGameCam;
+    [SerializeField] CinemachineVirtualCamera TimelineCam;
+    public CinemachineConfiner InGameConfiner;
+    public CinemachineConfiner TimeLineConfiner;
+    void Awake()
+    {
+        instance = this;
+        isInGame = true;
+    }
+
+    public void ChangeCam(Collider2D border)
+    {
+        if(instance.isInGame)
+        {
+            instance.TimeLineConfiner.m_BoundingShape2D = border;
+            instance.isInGame = false;
+            instance.TimelineCam.gameObject.SetActive(true);
+            instance.InGameCam.gameObject.SetActive(false);
+
+
+            //instance.ShakeCamera(0f, 0f);
+        }
+        else
+        {
+            instance.InGameConfiner.m_BoundingShape2D = border;
+            instance.isInGame = true;
+            instance.InGameCam.gameObject.SetActive(true);
+            instance.TimelineCam.gameObject.SetActive(false);
+
+
+            //instance.ShakeCamera(0f, 0f);
+        }
+    }
+
+    public void ShakeCamera(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin multiChannelPerlin;
+        if(isInGame)
+        {
+            multiChannelPerlin = InGameCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        }
+        else 
+        {
+            multiChannelPerlin = TimelineCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        }
+        
+        multiChannelPerlin.m_AmplitudeGain = intensity;
+
+        startingIntensity = intensity;
+        totalShakeTimer = time;
+        shakeTimer = time;
+    }
+
+    private void Update() 
+    {
+        if(shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            if(shakeTimer <= 0f)
+            {   
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin;
+                if(isInGame)
+                {
+                    cinemachineBasicMultiChannelPerlin = InGameCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                }
+                else
+                {
+                    cinemachineBasicMultiChannelPerlin = TimelineCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                }
+                
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 
+                    Mathf.Lerp(startingIntensity, 0f, (1 - (shakeTimer / totalShakeTimer)));
+            }
+        }
+    }
+}
+
+
+*/
---
+++ Project/Assets/Dev/Husk/Script/Camera/SwitchCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D IngameConfiner;
+    [SerializeField] PolygonCollider2D TimelineConfiner;
+    [SerializeField] CameraManager cameraManager;
+
+
+    private void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            if(cameraManager.isInGame && !cameraManager.tabbed)
+            {
+                cameraManager.ChangeCam(TimelineConfiner);
+            }
+            else
+            {
+                cameraManager.ChangeCam(IngameConfiner);
+            } 
+        }
+    }
+
+    
+}
