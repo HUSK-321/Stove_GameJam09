3a53b2f81d04cb5fd5fd7185288340f643ed11a3
pengzer1 <81570534+pengzer1@users.noreply.github.com>
2021-09-25 04:53:42 +0900

    Update SliderMove.cs

--- Project/Assets/Dev/Cha/Script/SliderMove.cs
+++ Project/Assets/Dev/Cha/Script/SliderMove.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class SliderMove : MonoBehaviour
 {
-    public Vector2 startPos;
-    public Vector2 endPos;
     SpriteRenderer SpriteRenderer;
     Rigidbody2D rigid;
-    public Transform Target;
-    public float Speed = 1f;
     // Start is called before the first frame update
     void Awake()
     {
-        startPos = new Vector2(-6.5f, -14.55f);
-        endPos = new Vector2(8f, -14.55f);
         rigid = GetComponent<Rigidbody2D>();
         SpriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -45,36 +39,12 @@ public class SliderMove : MonoBehaviour
     }
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateMove(startPos, endPos, 10f);
     }
 
-    void UpdateMove(Vector2 startPos, Vector2 targetPos, float duration)
-    {
-        float timer = 0f;
 
-        // 이동 시작 위치 설정
-        Vector2 position = startPos;
-        rectTransform.anchoredPosition = position;
-
-        // 시간에 따른 위치 설정
-        while (timer < duration)
-        {
-            timer += Time.deltaTime;
-
-            position.x = Mathf.Lerp(startPos.x, targetPos.x, timer / duration);
-            position.y = Mathf.Lerp(startPos.y, targetPos.y, timer / duration);
-
-            rectTransform.anchoredPosition = position;
-        }
-
-        // 이동 종료 위치 설정
-        position = targetPos;
-        rectTransform.anchoredPosition = position;
-    }
 }
