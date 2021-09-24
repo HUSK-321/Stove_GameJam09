951cc9159b3a37b6862c1017e40af5f460edd9b1
husk <huskgit@gmail.com>
2021-09-25 04:10:43 +0900

    타이머 생성

---
+++ Project/Assets/Dev/Husk/Script/Timer/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public static Timer instance;
+    const float maxTime = 10f;
+    public float currentTime = 0f;
+    [SerializeField] CameraManager cameraManager;
+
+    // UI
+    [SerializeField] TextMeshProUGUI timer;
+
+    private void Awake() 
+    {
+        instance = this;
+        currentTime = maxTime;
+    }
+
+    private void Update() 
+    {
+        if(cameraManager.isInGame && currentTime > 0)
+        {
+            currentTime -= 1 * Time.deltaTime;
+            timer.text = currentTime.ToString() + " s";
+            
+        }
+        //  TODO : 플레이어 사망 로직 연결
+        if(currentTime <= 0)
+        {
+            timer.text = "0 s";
+            print("아이고난!");
+        }
+            
+        
+        print(currentTime);
+    }
+}
