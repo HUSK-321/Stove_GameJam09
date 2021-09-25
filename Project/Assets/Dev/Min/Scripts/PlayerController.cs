3266f5de6e942eab7ca598d6769466ae5f7d8101
martin-newbie <g.martin.min@gmail.com>
2021-09-25 10:10:49 +0900

    add player idle and invincible
    press space to start

--- Project/Assets/Dev/Min/Scripts/PlayerController.cs
+++ Project/Assets/Dev/Min/Scripts/PlayerController.cs
@@ -34,22 +34,25 @@ namespace Martin
         {
             IsGround,
             IsJump,
-            IsHover
+            IsHover,
+            IsInvincible,
+            IsIdle
         };
 
         AnimationType animType;
 
-        bool[] PlayerAnimSet; // 0 = ground, 1 = jump, 2 = hover
+        bool[] PlayerAnimSet; // 0 = ground, 1 = jump, 2 = hover, 3 = invincible, 4 = idle
         bool isStop;
         Vector2 velocity;
 
         void Start()
         {
+            CM = FindObjectOfType(typeof(CameraManager)) as CameraManager;
             anim = GetComponent<Animator>();
             RB = GetComponent<Rigidbody2D>();
-            animType = AnimationType.IsGround;
-            CM = FindObjectOfType(typeof(CameraManager)) as CameraManager;
-            PlayerAnimSet = new bool[3];
+            PlayerAnimSet = new bool[5];
+            animType = AnimationType.IsIdle;
+            AnimationBoolSet((int)animType);
         }
 
         void Update()
@@ -64,9 +67,15 @@ namespace Martin
                 case AnimationType.IsHover:
                     AnimationBoolSet(2);
                     break;
+                case AnimationType.IsInvincible:
+                    AnimationBoolSet(3);
+                    break;
+                case AnimationType.IsIdle:
+                    AnimationBoolSet(4);
+                    break;
             }
 
-            if (Input.GetKeyDown(KeyCode.Space)/* && CM != null && CM.isInGame*/)
+            if (Input.GetKeyDown(KeyCode.Space)/*&& CM.isInGame*/)
             {
                 Jump();
             }
@@ -74,6 +83,10 @@ namespace Martin
             {
                 isStop = !isStop;
                 RB.velocity = velocity;
+            }
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                Invincible();
             }
 
             // only activate in ingame scene
@@ -91,6 +104,8 @@ namespace Martin
             }
 
             AnimationSetting();
+
+            //UnityEditor.EditorApplication.isPaused = true;
         }
 
         void AnimationBoolSet(int idx)
@@ -107,6 +122,8 @@ namespace Martin
             anim.SetBool("IsGround", PlayerAnimSet[0]);
             anim.SetBool("IsJump", PlayerAnimSet[1]);
             anim.SetBool("IsHover", PlayerAnimSet[2]);
+            anim.SetBool("IsInvincible", PlayerAnimSet[3]);
+            anim.SetBool("IsIdle", PlayerAnimSet[4]);
         }
 
         public void Hover()
@@ -117,10 +134,14 @@ namespace Martin
 
         public void Jump()
         {
-            if (PlayerAnimSet[0])
+            if (PlayerAnimSet[0] || PlayerAnimSet[4])
             {
                 animType = AnimationType.IsJump;
                 AnimationBoolSet(1);
+                RB.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+            }
+            else if(PlayerAnimSet[3])
+            {
                 RB.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
             }
         }
@@ -128,12 +149,16 @@ namespace Martin
         public void Ground()
         {
             animType = AnimationType.IsGround;
-            AnimationBoolSet(0);
+        }
+
+        public void Invincible()
+        {
+            animType = AnimationType.IsInvincible;
         }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if (other.gameObject.CompareTag("Ground"))
+            if (other.gameObject.CompareTag("Ground") && !PlayerAnimSet[3] && !PlayerAnimSet[4])
             {
                 Ground();
             }
