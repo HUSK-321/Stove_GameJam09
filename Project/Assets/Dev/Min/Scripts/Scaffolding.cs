55856b0bcae243413e1293a3b809fdb344ed9bb9
martin-newbie <g.martin.min@gmail.com>
2021-09-25 23:42:55 +0900

    add and fix stage 2-7, 8, 9, 10
    and add scaffolding

---
+++ Project/Assets/Dev/Min/Scripts/Scaffolding.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaffolding : MonoBehaviour
+{
+
+    BoxCollider2D box;
+
+    void Start()
+    {
+        box = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.CompareTag("PlayerHead"))
+        {
+            box.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            box.isTrigger = false;
+        }
+    }
+}
